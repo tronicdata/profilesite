a811b00f0038ee00f79720e93d9496a83344dc0d
tronicdata <larrybuffaloboy@gmail.com>
2015-02-16 23:48:18 -0500

    "few css adjustements, and failed attempt to retrieve wp blog posts. placeholder until design changes."

--- public/shared/js/script.js
+++ public/shared/js/script.js
@@ -1,4 +1,15 @@
 console.log("Hello, World");
+var xmlhttp;
+if (window.XMLHttpRequest){
+	// code for IE7+, Firefox, Chrome, Opera, Safari
+	xmlhttp=new XMLHttpRequest();
+} else {
+	// code for IE6, IE5
+	xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+}
+xmlhttp.open("GET", "https://larrybuffaloboy.wordpress.com/posts", true);
+xmlhttp.send();
+
 $(document).ready(function(){
 	console.log("hello, ready");
 
